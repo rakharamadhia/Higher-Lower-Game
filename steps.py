fc6b95cc0f82cff69cd37572b42f24d91f7e6901
Rakha R. Putra <rakharamadhia@gmail.com>
2023-03-05 04:40:35 +0000

    add steps

---
+++ steps.py
@@ -0,0 +1,21 @@
+# Display art
+
+# Generate a random account from the game data
+
+# Format the data into printable format 
+
+# Ask user for a guess
+
+# Check if user is correct
+## Get Follower count for each account
+## Use if statement to check the user guesses
+
+# Give user the feedback ont their guess
+
+# Score Keeping
+
+# Make the game repeatable
+
+# Making account position B became the next account at position A
+
+# Clear the screens between rounds
